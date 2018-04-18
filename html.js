2f10be9e0d76f668b3ae931987efaed01e70c4f2
keviljh3 <kevinljh3@hotmail.com>
2018-04-19 04:16:14 +0800

    Update html.js

--- html.js
+++ html.js
@@ -1,26 +1,16 @@
 var http = require('http');
-var url1 = require('url');
 
     http.createServer(function(req, res){ 
         var time=new Date();	
         var html = '<html>'  
         +'<head>'  
-        +'<title>DaoCloud Nodejs Reboot</title>'  
+        +'<title>jel Nodejs 80</title>'  
         +'</head>'  
         +'<body>'  
-		+ time+'<br>';
-		var cbool='Fail';
-        try{
-		var arg = url1.parse(req.url, true).query;
-		if (arg.rb=='true')
-		{
-          cbool='Success';
-		}
-		}catch (err){}
-		html=html+   'Reboot DaoCloud '+cbool+' !' 
+		+ time+'<br>'
         +'</body>'  
         +'</html>';  
         res.writeHead(200,{'Content-Type' : 'text/html'});  
         res.write(html);  
         res.end();  
-    }).listen(18888);  
+    }).listen(80); 
