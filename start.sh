2d23f3ea03c926b78e3589ab001a5c77529c6724
keviljh3 <kevinljh3@hotmail.com>
2018-04-19 05:42:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ nohup node html.js > nodejs.log 2>&1 &
 cd /fs
 sh restart.sh
 cd /ssr/shadowsocks
-nohup python server.py -p 17520 -k Ssr123456 -m aes-256-cfb -O auth_aes128_sha1 -o tls1.2_ticket_auth_compatible -G 32 -g www.bing.com > ssr.log 2>&1 &
+nohup python server.py -p 17520 -k Ssr123456 -m chacha20-ietf -O auth_aes128_sha1 -o tls1.2_ticket_auth_compatible -G 32 -g www.bing.com > ssr.log 2>&1 &
