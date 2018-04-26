f4088f47d41caf35be8db57c5c517e7802ed2f08
keviljh3 <kevinljh3@hotmail.com>
2018-04-26 09:27:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 killall server_linux_amd64
 killall python                                                                                                  
 killall node
+ulimit -n 65535
 nohup ./server_linux_amd64 -t "0.0.0.0:17520" -l ":17518" -key test -mtu 1350 -sndwnd 610 -rcvwnd 610 -crypt none -mode fast2 -dscp 0 -datashard 10 -parityshard 3 -nocomp> kcptun.log 2>&1 &
 nohup node html.js > nodejs.log 2>&1 &
 cd /fs
